0fbbb582271919ac0863d5e5935dc95d494611b3
kaiwu <kawu6742@uni.sydney.edu.au>
2021-09-22 16:33:43 +0800

    initialization

---
+++ src/main/java/com/kai/dao/ProductMapper.java
@@ -0,0 +1,17 @@
+package com.kai.dao;
+
+import com.kai.pojo.Product;
+
+public interface ProductMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Product record);
+
+    int insertSelective(Product record);
+
+    Product selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Product record);
+
+    int updateByPrimaryKey(Product record);
+}
